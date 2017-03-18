3b2c0203ccc4bf2161d57945888cf6d61e9e1080
Carlos Eduardo Ferrari <carled@gmail.com>
2017-03-17 22:26:47 -0300

    Commit Inicial

---
+++ LoginAndroid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace LoginAndroid
+{
+    [Activity(Label = "LoginAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+
+        public void onClick(View v)
+        {
+            
+        }
+    }
+}
